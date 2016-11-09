ef94d061c1aff34a2707f41ada719a0520eb73ba
Гаврилова Иван <discover1977@yandex.ru>
2016-11-10 00:47:18 +0300

    +

--- ws2812.h
+++ ws2812.h
@@ -14,8 +14,8 @@
 // These lines specify what pin the LED strip is on.
 // You will either need to attach the LED strip's data line to PC0 or change these
 // lines to specify a different pin.
-#define LED_STRIP_PORT PORTC
-#define LED_STRIP_DDR  DDRC
+#define LED_STRIP_PORT PORTA
+#define LED_STRIP_DDR  DDRA
 #define LED_STRIP_PIN  0
 
 /** The rgb_color struct represents the color for an 8-bit RGB LED.
